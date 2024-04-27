c6495e431a20bf6ac7f0561313ea3d16b074f531
LaptopmanMann <florianagario2@gmail.com>
2024-04-27 11:41:21 +0200

    fixed the shop

--- src/hypixelCosmetic/cosmeticInventory.ts
+++ src/hypixelCosmetic/cosmeticInventory.ts
@@ -8,6 +8,8 @@ import { ActionFormData, ModalFormData } from "@minecraft/server-ui";
 import { isNumber } from "util";
 import "../hypixelFunctions/playerFunctions";
 import { Interface } from "readline";
+import { askForConfirmation } from "hud";
+import { LinkedList } from "dataTypes/linkedList";
 
 
 const EnumKeys = Object.keys(ECosmeticType).filter(key =>  isNaN(Number(key)));
@@ -96,6 +98,7 @@ class PlayerCosmetic {
 
     lockCosmetic = (cosmeticId : keyof CosmeticId | string) => {
         this.player.setDynamicProperty(`${cosmeticId}`, false);
+        this.player.setDynamicProperty(`empty`, true)
         //Remove from saved and set the players cosmetic to empty if it is locked
     }
 
@@ -110,6 +113,7 @@ class PlayerCosmetic {
         for(const cosmetic of cosmeticList){
             this.player.setDynamicProperty(`${cosmetic.cosmeticId}`, false);
         }
+            this.player.setDynamicProperty(`empty`, true)
         for(const key of EnumKeys){
             this.player.setDynamicProperty(`saved${key}`, "empty");
             this.cosmetic[ECosmeticType[key]] = getCosmeticById("empty");
@@ -117,15 +121,20 @@ class PlayerCosmetic {
     }
 
     cosmeticShop = () => {
+        
         const cosmeticShop = new ActionFormData();
         cosmeticShop.title("Cosmetics");
         
         const playerGold = this.player.getHypixelValue("winsCurrency");
         cosmeticShop.body("Select a cosmetic to buy: §a" + playerGold);
+        let cosmeticsLeftToBuy = false;
+        const buyableCosmetics : LinkedList<ICosmetic> = new LinkedList<ICosmetic>();
         for(const cosmetic of cosmeticList){
             if(this.player.getDynamicProperty(`${cosmetic.cosmeticId}`) as boolean){
                 continue;
             }
+            cosmeticsLeftToBuy = true;
+            buyableCosmetics.append(cosmetic);
             if(this.player.getHypixelValue("winsCurrency") >= cosmetic.cost){
                 cosmeticShop.button(`${cosmetic.cosmeticId} \n§aCost: ${cosmetic.cost}`);
             } 
@@ -133,11 +142,34 @@ class PlayerCosmetic {
                 cosmeticShop.button(`${cosmetic.cosmeticId} \n§cCost: ${cosmetic.cost}`);    
             }
         }
+        if(!cosmeticsLeftToBuy){
+            this.player.sendMessage("All cosmetics have been purchased!");
+            return;
+        }
         showHUD(this.player, cosmeticShop).then((response) => {
             if(response.canceled){
                 return;
             }
             
+            if(buyableCosmetics.getNodebyIndex(response.selection).data.cost > this.player.getHypixelValue("winsCurrency")){
+                this.player.sendMessage("You don't have enough gold!");
+                return;
+                
+            }
+            else {
+                askForConfirmation(this.player, `Are you sure you want to buy ${buyableCosmetics.getNodebyIndex(response.selection).data.cosmeticId}?`).then((res) => {
+                    if(res){
+                        this.unlockCosmetic(buyableCosmetics.getNodebyIndex(response.selection).data.cosmeticId);
+                        this.player.setHypixelValue("winsCurrency", this.player.getHypixelValue("winsCurrency") - buyableCosmetics.getNodebyIndex(response.selection).data.cost);
+                        console.log(`Purchased ${buyableCosmetics.getNodebyIndex(response.selection).data.cosmeticId} for ${buyableCosmetics.getNodebyIndex(response.selection).data.cost} by ${this.player.name}`);
+                    }
+                    askForConfirmation(this.player, "Do you want to equip this cosmetic?").then((res2) => {
+                        if(res2){
+                            this.setCosmetic(buyableCosmetics.getNodebyIndex(response.selection).data.cosmeticId, buyableCosmetics.getNodebyIndex(response.selection).data.cosmeticType);
+                        }
+                    })
+                })
+            }
         })
     }
 }
